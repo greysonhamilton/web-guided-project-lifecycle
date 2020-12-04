0e5661691630b8e0a1528091069d3f10b4bdb038
Greyson <greyson.r.hamilton@gmail.com>
2020-12-03 21:41:49 -0500

    working doggo app

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -16,6 +16,36 @@ class App extends React.Component {
     
     }
 
+    handleChanges = (e) => {
+
+        console.log(e.target.value);
+        this.setState({
+            ...this.state,
+            dogBreed: e.target.value
+
+        })
+
+    }
+
+    fetchDoggos = () => {
+
+        alert(`Fetching ${this.state.dogBreed} dogs.`);
+
+        axios.get(`https://dog.ceo/api/breed/${this.state.dogBreed}/images`)
+
+        .then((res) => {
+            this.setState({
+                ...this.state,
+                doggos: res.data.message
+            });
+        })
+
+        .catch((err) => {
+            console.log(err);
+        })
+
+    }
+
     componentDidMount() {
 
         axios.get('https://dog.ceo/api/breed/hound/images')
@@ -35,6 +65,28 @@ class App extends React.Component {
 
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        // runs when state or props have been updated
+        // always use an if statement to prevent infinite loops
+        if (prevState.doggos!== this.state.doggos) {
+            console.log('doggos have changed')
+        }
+
+        if (this.state.dogBreed === 'chihuahua') {
+            axios.get(`https://dog.ceo/api/breed/mastiff/images`)
+            .then((res) => {
+                console.log();
+                this.setState({
+                    ...this.state,
+                    doggos: res.data.message
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+        }
+    }
+
     render() {
         
         return (
@@ -47,6 +99,13 @@ class App extends React.Component {
                 <h2>
                     Here are some Doggos!
                 </h2>
+                <input 
+                    placeholder='Dog breed?' 
+                    value={this.state.dogBreed} //read value from component state
+                    type='text' 
+                    onChange={this.handleChanges} //write to Component state
+                 />
+                 <button onClick={this.fetchDoggos}>Fetch Doggos</button>
                 <div>
                     {this.state.doggos.map((doggo) => {
                     return <img width='200' className='doggo' src={doggo} key = {doggo} />})}
